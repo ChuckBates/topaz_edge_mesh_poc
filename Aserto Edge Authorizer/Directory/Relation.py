c08f71a38fb1730dea8bfe6a57082d61235a7ff1
Chuck <chuck.bates@transport4.com>
2025-02-28 16:54:26 -0700

    add tests for relations

--- Aserto Edge Authorizer/Directory/Relation.py
+++ Aserto Edge Authorizer/Directory/Relation.py
@@ -15,11 +15,22 @@ class Relation:
         return relation
 
     def delete_relation(self, object_type, object_id, relation, subject_type, subject_id):
-        relation = self.directory_connection.delete_relation(
+        try:
+            self.directory_connection.get_relation(
+                object_type=object_type,
+                object_id=object_id,
+                relation=relation,
+                subject_type=subject_type,
+                subject_id=subject_id
+            )
+        except Exception as e:
+            return "Relation " + object_id + " - " + subject_id + " not found"
+        
+        self.directory_connection.delete_relation(
             object_type=object_type,
             object_id=object_id,
             relation=relation,
             subject_type=subject_type,
             subject_id=subject_id
         )
-        return relation
+        return "Relation " + object_id + " - " + subject_id + " deleted"
---
+++ Aserto Edge Authorizer/tests/directory_tests/test_relation.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from directory.relation import Relation
+
+def test_create_relation(mocker):
+    object_type = "object_type"
+    object_id = "object_id"
+    relation = "relation"
+    subject_type = "subject_type"
+    subject_id = "subject_id"
+
+    mock_directory_connection = mocker.Mock()
+    mock_directory_connection.set_relation.return_value = {}
+    relation = Relation(mock_directory_connection)
+
+    result = relation.create_relation(object_type, object_id, relation, subject_type, subject_id)
+
+    assert result == {}
+    mock_directory_connection.set_relation.assert_called_once_with(
+        object_type=object_type,
+        object_id=object_id,
+        relation=relation,
+        subject_type=subject_type,
+        subject_id=subject_id
+    )
+
+def test_delete_relation_and_the_relation_is_not_found(mocker):
+    object_type = "object_type"
+    object_id = "object_id"
+    relation = "relation"
+    subject_type = "subject_type"
+    subject_id = "subject_id"
+
+    mock_directory_connection = mocker.Mock()
+    mock_directory_connection.get_relation.side_effect = Exception("Relation not found")
+    relation = Relation(mock_directory_connection)
+
+    result = relation.delete_relation(object_type, object_id, relation, subject_type, subject_id)
+
+    assert result == "Relation " + object_id + " - " + subject_id + " not found"
+    mock_directory_connection.get_relation.assert_called_once_with(
+        object_type=object_type,
+        object_id=object_id,
+        relation=relation,
+        subject_type=subject_type,
+        subject_id=subject_id
+    )
+
+def test_delete_relation_and_the_relation_is_found(mocker):
+    object_type = "object_type"
+    object_id = "object_id"
+    relation_type = "relation"
+    subject_type = "subject_type"
+    subject_id = "subject_id"
+
+    mock_relation_return = type('', (object,), {'id': object_id})()
+    mock_directory_connection = mocker.Mock()
+    mock_directory_connection.get_relation.return_value = mock_relation_return
+    mock_directory_connection.delete_relation.return_value = {}
+    relation = Relation(mock_directory_connection)
+
+    result = relation.delete_relation(object_type, object_id, relation_type, subject_type, subject_id)
+
+    assert result == "Relation " + object_id + " - " + subject_id + " deleted"
+    mock_directory_connection.get_relation.assert_called_once_with(
+        object_type=object_type,
+        object_id=object_id,
+        relation=relation_type,
+        subject_type=subject_type,
+        subject_id=subject_id
+    )
+    mock_directory_connection.delete_relation.assert_any_call(
+        object_type=object_type,
+        object_id=object_id,
+        relation=relation_type,
+        subject_type=subject_type,
+        subject_id=subject_id
+    )
+    
